7cd2596aea5941053dc6cd05fa69d5aa7db8ab4b
alexcomu <alex.comunian@gmail.com>
2016-11-14 17:51:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# BANK API - Flask Project
+
+## Requirements
+
+You'll need MongoDB and Flask.
+
+## Install
+
+Clone the repo, create a virtualenv and install the requirements:
+
+    git clone https://github.com/alexcomu/bank-api-flask.git
+    cd bank-api-flask
+    virtualenv envFlask
+    source envFlask/bin/activate
+    pip install -r requirements
+
+## Run
+
+Just:
+
+    python app.py
+
+## Route
+
+Only 3 routes:
+
+    http://localhost:5000/ 
+    http://localhost:5000/banks
+    http://localhost:5000/data/BANK_ID
+
---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import redirect, Flask, jsonify, abort, request, make_response, url_for
+from pymongo import MongoClient
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.bank
+
+class Bank(Resource):
+    def get(self):
+        data = db.bank.find({},{"_id": 0})
+        banks = [b for b in data]
+        return jsonify( { 'data': banks } )
+
+class Data(Resource):
+    def get(self, bank_id=None):
+        bank = db.bank.find_one({"BANK_ID": bank_id}, {"_id": 0})
+        if not bank:
+            return redirect(url_for("bank"))
+        data = db.data.find({"BANK": bank['BANK']},{"_id": 0})
+        response = [d for d in data]
+        return jsonify( { 'data': response, 'bank': bank } )
+
+class Index(Resource):
+    def get(self):
+        return redirect(url_for("bank"))
+
+api = Api(app)
+api.add_resource(Index, "/", endpoint="index")
+api.add_resource(Bank, "/banks")
+api.add_resource(Data, "/data/<int:bank_id>")
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
